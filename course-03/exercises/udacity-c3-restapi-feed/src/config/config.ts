4ba869308586021a0081a396ebc3d0f04bcf08df
abhinavk1492 <abhinavk1492@gmail.com>
2019-09-24 18:07:38 -0400

    added course-03

---
+++ course-03/exercises/udacity-c3-restapi-feed/src/config/config.ts
@@ -0,0 +1,23 @@
+export const config = {
+  "dev": {
+    "username": "udagramkapuladev",
+    "password": "udagramkapuladev",
+    "database": "udagramkapuladev",
+    "host": "udagramkapuladev.cufw7brhsxxp.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_reigion": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-kapula-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+
+}
